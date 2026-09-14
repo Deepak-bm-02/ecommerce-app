976d029867be35ebfe70e78a7a3168dbdc33ef66
Deepak <deepakgowdadeepak56@gmail.com>
2026-09-14 23:10:35 +0530

    Add products GET API

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,30 @@ const app = express();
 
 const PORT = 5000;
 
+const products = [
+    {
+        id: 1,
+        name: "Laptop",
+        price: 55000
+    },
+    {
+        id: 2,
+        name: "Headphones",
+        price: 2500
+    },
+    {
+        id: 3,
+        name: "Smartphone",
+        price: 30000
+    }
+];
+
 app.get("/", (req, res) => {
     res.send("E-Commerce API is running");
+});
+
+app.get("/api/products", (req, res) => {
+    res.json(products);
 });
 
 app.listen(PORT, () => {
